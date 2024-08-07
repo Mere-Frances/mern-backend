5047029abcb2cb09504a2e61a64837b96a5e05e4
Mere Frances <meredithflss@gmail.com>
2024-08-08 11:24:36 +1200

    allowed cors

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ const app = express();
 // set a port - listen changes on the port
 const port = 4000;
 
+// allow Cross Origin
+app.use(cors());
+
 // import routes
 const workoutRoutes = require('./routes/workouts')
 
